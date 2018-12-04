154f80e986c34fe53c7061b14800dd6e3777d7cb
Raghu K <raghuk.vit@gmail.com>
2018-12-04 01:31:50 +0000

    Updating stage-failure-handle2

---
+++ jobs/stage-failure-handle2/Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+node() {
+    stage('STAGE1'){
+        sh 'exit 0'
+    }
+    try {
+        stage('STAGE2'){
+            sh 'exit 1'
+        }
+    }catch(e) {
+        build_ok = false
+    }
+    if(build_ok) {
+        stage('STAGE3'){
+            sh 'exit 0'
+        }
+    } else {
+        stage('STAGE4'){
+            sh 'exit 0'
+        }
+    }
+}
