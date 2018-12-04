1cb1ade3cf1eb310a64d72a981856e6448754b98
Raghu K <raghuk.vit@gmail.com>
2018-12-04 01:24:44 +0000

    Updating jobs

--- jobs/stage-failure-handle1/Jenkinsfile.groovy
+++ jobs/stage-failure-handle1/Jenkinsfile.groovy
@@ -2,8 +2,12 @@ node() {
     stage('STAGE1'){
         sh 'exit 0'
     }
-    stage('STAGE2'){
-        sh 'exit 1'
+    try {
+        stage('STAGE2'){
+            sh 'exit 1'
+        }
+    }catch(e) {
+        print "LEts continue"
     }
     stage('STAGE3'){
         sh 'exit 0'
